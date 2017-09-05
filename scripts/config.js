42bd2b8579fdb618dcdc69f941158ea3841e9299
Hayat Mazzarella <imhighyat@gmail.com>
2017-09-05 11:20:29 -0700

    deleted gitignore for now

---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+
+// ENDPOINTS
+const CHAR_SEARCH = "https://gateway.marvel.com/v1/public/characters"; //marvel endpoint
+const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"; //youtube endpoint
+
+
+// AUTHENTICATIONS
+const MARVEL_API_AUTH = {
+	KEY: "58149727a1ccb3e2d0983beb68c219ac",
+	HASH: "9537e876fc1cc632440c7e9b3ab3fe7e"
+};
+
+const YOUTUBE_API_AUTH = "AIzaSyClYmxQUfYLWr81myf1BaGDeAuGC-AmV0o";
